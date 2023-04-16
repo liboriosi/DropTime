17afe2db4f4f6a2511a9295b0e65b0c94de1a4f9
Gabriel Libório <liborio@edu.univali.br>
2023-04-15 23:24:25 -0300

    Implementa layout das telas de login e cadastro
    Implemendatado layout da tela de login, recuperação de senha, cadastro, dados pessoais para cadastro, e splash page;
    
    Adicionada a biblioteca do material design para que seja possivel utilizar os widget da biblioteca.

---
+++ app/src/main/java/com/mobile/tabalho/droptime/CadastroFragment.java
@@ -0,0 +1,32 @@
+package com.mobile.tabalho.droptime;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class CadastroFragment extends Fragment {
+
+    public CadastroFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cadastro, container, false);
+    }
+}
